a6b438bd23494f6d28eb3997241e1a697c177b4d
MosadMohamed <54008216+MosadMohamed@users.noreply.github.com>
2021-02-09 21:54:08 +0200

    Mosad

--- app/Http/APIs/PdfController.php
+++ app/Http/APIs/PdfController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\APIs;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class PdfController extends Controller
 {
--- routes/api.php
+++ routes/api.php
@@ -20,12 +20,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 Route::group([ 'middleware' => ['api'],'prefix' => 'user'], static function ($router) {
-    Route::post('/login', 'UserController@authenticate');
-    Route::post('/register', 'UserController@register');
-    Route::group(['middleware' => 'jwt.verify'], static function( $router){
-        Route::post('/logout', 'UserController@logout');
-        Route::post('/refresh', 'UserController@refresh');
-        Route::get('/detail', 'UserController@detail'); });
+  Route::post('/login', 'UserController@authenticate');
+  Route::post('/register', 'UserController@register');
+  Route::group(['middleware' => 'jwt.verify'], static function( $router){
+    Route::post('/logout', 'UserController@logout');
+    Route::post('/refresh', 'UserController@refresh');
+    Route::get('/detail', 'UserController@detail'); });
 });
 
 
@@ -33,26 +33,22 @@ Route::group([ 'middleware' => ['api'],'prefix' => 'user'], static function ($ro
 /* ------------------- localhost:8080/users/getusers----------------- */
 /**-----------------------users endpoint ------------------------------------- */
 Route::group(['prefix' => 'users'], function () {
-    Route::get('/', 'UserController@getAll')->name('getusers');       
-    Route::get('/{id}', 'UserController@get')->name('getuser');       
-    Route::post('/', 'UserController@create')->name('createuser');
-    Route::put('/{id}', 'UserController@update')->name('updateuser');
-    Route::delete('/{id}', 'UserController@delete')->name('deleteuser');
-  });
+  Route::get('/', 'UserController@getAll')->name('getusers');       
+  Route::get('/{id}', 'UserController@get')->name('getuser');       
+  Route::post('/', 'UserController@create')->name('createuser');
+  Route::put('/{id}', 'UserController@update')->name('updateuser');
+  Route::delete('/{id}', 'UserController@delete')->name('deleteuser');
+});
 
 
 /* ---------------------- items endpoint --------------------- */
-  Route::group(['prefix'=>'products'], function () {
-    Route::get('/', 'ProductController@getAll');            //http://127.0.0.1:8000/api/products/getitems
-    Route::get('/{id}', 'ProductController@getOne');        //http://127.0.0.1:8000/api/products/getitem
-    Route::post('/', 'ProductController@create');           //http://127.0.0.1:8000/api/products/createitem
-    Route::put('/{id}', 'ProductController@update');        //http://127.0.0.1:8000/api/products/updateitem
-    Route::delete('/{id}', 'ProductController@delete');     //http://127.0.0.1:8000/api/products/deleteitem
-  });
-
-
-
-
+Route::group(['prefix'=>'products'], function () {
+  Route::get('/', 'ProductController@getAll');            //http://127.0.0.1:8000/api/products/getitems
+  Route::get('/{id}', 'ProductController@getOne');        //http://127.0.0.1:8000/api/products/getitem
+  Route::post('/', 'ProductController@create');           //http://127.0.0.1:8000/api/products/createitem
+  Route::put('/{id}', 'ProductController@update');        //http://127.0.0.1:8000/api/products/updateitem
+  Route::delete('/{id}', 'ProductController@delete');     //http://127.0.0.1:8000/api/products/deleteitem
+});
 
 
 /* ---------------------- pdf --------------------- */
